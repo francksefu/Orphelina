b0553857763d036cebf401b3800e5f6c54892d5d
francksefu <francksefu1998@gmail.com>
2024-08-02 09:56:45 +0200

    Remote test

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
+// for local test
     /*$host = 'localhost';
     $db = 'orphelina';
     $user = 'root';
     $password = 'root';*/
+//for remote
     $host = 'localhost';
     $db = 'u471236973_compassion';
     $user = 'u471236973_compassion';
